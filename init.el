3980f7fecd7544c2fdb3d4c6d86714c7b560af0f
menxit <federico@menxit.com>
2018-07-17 18:21:04 +0200

    helm everywhere

--- init.el
+++ init.el
@@ -18,6 +18,12 @@
 
 ;; helm
 (require 'helm-config)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-m") 'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-h f") 'helm-apropos)
 
 ;; packages
 (custom-set-variables
