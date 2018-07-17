92792532e98b2a6398aa7e509f2ab050beb28289
menxit <federico@menxit.com>
2018-07-17 13:44:28 +0200

    special characters

--- init.el
+++ init.el
@@ -1,7 +1,3 @@
-;; Italian keyboard
-(defun aq-binding (any) nil)
-(load  "~/.emacs.d/aquamacs-tools.el")    
-(load  "~/.emacs.d/emulate-mac-keyboard-mode.el")
-(setq emulate-mac-finnish-keyboard-mode t)
-(setq mac-right-option-modifier nil)
-
+;; special characters using right alt
+(setq ns-alternate-modifier 'meta)
+(setq ns-right-alternate-modifier 'none)
