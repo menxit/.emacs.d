09754510d15387cf3d8e3aff4dd937246daed5bc
menxit <federico@menxit.com>
2018-07-19 09:35:31 +0200

    light theme

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # How to install
 
 ```bash
+npm i -g typescript
 rm -rf .emacs.d
 git clone --recurse-submodules -j8 git@github.com:menxit/.emacs.d.git
 ```
--- init.el
+++ init.el
@@ -1,8 +1,28 @@
 ;; packages repo
-(package-initialize)
+(require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
   ("marmalade" . "http://marmalade-repo.org/packages/")
   ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+;; list the packages you want
+(setq package-list
+    '(helm company magit twilight-bright-theme))
+
+;; activate all the packages
+(package-initialize)
+
+;; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; load theme
+(require 'twilight-bright-theme)
+(load-theme twilight-bright t)
 
 ;; special characters using right alt
 (setq ns-alternate-modifier 'meta)
@@ -24,10 +44,50 @@
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 (global-set-key (kbd "C-h f") 'helm-apropos)
 
-;; packages
+;; path
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
+;; javascript
+
+;; typescript
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+(setq company-tooltip-align-annotations t)
+(add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+;; rename current buffer
+(defun rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+(global-set-key (kbd "C-c r")  'rename-file-and-buffer)
 (custom-set-variables
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(package-selected-packages (quote (helm))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (twilight-bright-theme rjsx-mode js2-mode magit company helm))))
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
